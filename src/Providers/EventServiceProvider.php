bcb67debfb11ab23c430afa9a4a8c67c44bb9aff
zhangyubo <zhangyubo@192.168.99.1>
2017-05-25 10:40:10 +0800

    1.1.20

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -1,30 +1,27 @@
 <?php
 
-namespace App\Providers;
+namespace GouuseCore\Providers;
 
 use Laravel\Lumen\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Database\Events\StatementPrepared;
-
 use Event;
-class EventServiceProvider extends ServiceProvider
-{
+
+class EventServiceProvider extends ServiceProvider {
 	/**
 	 * The event listener mappings for the application.
 	 *
 	 * @var array
 	 */
-	protected $listen = [
-			'App\Events\SomeEvent' => [
-					'App\Listeners\EventListener',
+	protected $listen = [ 
+			'App\Events\SomeEvent' => [ 
+					'App\Listeners\EventListener' 
 			],
-			'Illuminate\Database\Events\QueryExecuted' => [
-					'GouuseCore\Listeners\QueryListener'
-			]
+			'Illuminate\Database\Events\QueryExecuted' => [ 
+					'GouuseCore\Listeners\QueryListener' 
+			] 
 	];
-
-	public function boot()
-	{
-		Event::listen(StatementPrepared::class, function($event) {
+	public function boot() {
+		Event::listen ( StatementPrepared::class, function ($event) {
 			$event->statement->setFetchMode(\PDO::FETCH_ASSOC);
 		});
 	}

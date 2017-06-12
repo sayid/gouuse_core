452ef22e77effe6a59dc8fddaa8483c9cd83b821
zhangyubo <zhangyubo@192.168.5.126>
2017-06-12 15:34:58 +0800

    1.1.36

--- src/Swoole/Server.php
+++ src/Swoole/Server.php
@@ -23,7 +23,8 @@ class Server
 
     public function __construct($config, $swoole_settings = [])
     {
-        $this->swoole_http_server = new swoole_http_server($config['host'], $config['port']);
+    	
+        $this->swoole_http_server = new swoole_http_server($config['host'], $config['port'], $config['mode']);
         $this->pid_file = $config['pid_file'];
         $this->root_dir = $config['root_dir'];
         $this->deal_with_public = $config['deal_with_public'];

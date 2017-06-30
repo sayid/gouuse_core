539b47198065e824a595b43626f8f02bed9aaecf
zhangyubo <zhangyubo@169.254.40.224>
2017-06-30 14:25:58 +0800

    登录失败次数

---
+++ src/Libraries/FrequencyLib.php
@@ -0,0 +1,35 @@
+<?php
+namespace GouuseCore\Libraries;
+
+/**
+ * 访问频率限制
+ * @author zhangyubo
+ *
+ */
+class FrequencyLib extends Lib
+{
+	/**
+	 * 
+	 * @param unknown $url 频率限制的URL
+	 * @param unknown $key 限制的key值
+	 * @param number $life_time 有效时间
+	 */
+    public function set($url, $key, $life_time = 600)
+    {
+    	$value = $this->CacheLib->get(md5($url.'_'.$key));
+    	$value = intval($value) + 1;
+    	$this->CacheLib->set(md5($url.'_'.$key), $value, $life_time);
+    }
+    
+    public function check($url, $key, $times = 5)
+    {
+    	$value = $this->CacheLib->get(md5($url.'_'.$key));
+    	$value = intval($value) + 1;
+    	if ($value >= $times) {
+    		return false;
+    	}
+    	return true;
+    }
+    
+    
+}

13de4099e3c39692b3e335985969ebc94bd153bb
zhangyubo <zhangyubo@169.254.40.224>
2017-06-30 15:45:12 +0800

    1.1.64

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -5,6 +5,8 @@ namespace GouuseCore\Providers;
 use Laravel\Lumen\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Database\Events\StatementPrepared;
 use Event;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Events\QueryExecuted;
 
 class EventServiceProvider extends ServiceProvider {
 	/**
@@ -24,5 +26,11 @@ class EventServiceProvider extends ServiceProvider {
 		Event::listen ( StatementPrepared::class, function ($event) {
 			$event->statement->setFetchMode(\PDO::FETCH_ASSOC);
 		});
+		Event::listen ( QueryExecuted::class, function ($event) {
+			$sql = str_replace("?", "'%s'", $event->sql);
+			$log = vsprintf($sql, $event->bindings);
+			Log::info($log);
+		});
 	}
+	
 }

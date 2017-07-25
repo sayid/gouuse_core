0940fefea115cb6fc2cd5a7d7a3141b37aec9a79
zhangyubo <zhangyubo@192.168.5.136>
2017-07-25 16:58:50 +0800

    1.1.98

--- src/Rpcs/AuthCenterRpc.php
+++ src/Rpcs/AuthCenterRpc.php
@@ -17,12 +17,29 @@ class AuthCenterRpc extends BaseRpc
 		
 	}
 	
+	/**
+	 * 校验token
+	 * @param unknown $token
+	 * @return \GouuseCore\Rpcs\number[]|\GouuseCore\Rpcs\string[]|mixed
+	 */
 	function check($token) {
 		$url = $this->host . '/auth_center/v3/check';
 		$token = explode(' ', $token);
 		$token = end($token);
 		$header[] = 'Authorization: bearer '.$token;
 		$result = $this->postOutside($url, $header, []);
+		return $result;
+	}
+	
+	/**
+	 * 登录
+	 * @param string $account
+	 * @param string $password
+	 * @return \GouuseCore\Rpcs\number[]|\GouuseCore\Rpcs\string[]|mixed
+	 */
+	function login(string $account, string $password) {
+		$url = $this->host . '/auth_center/v3/login';
+		$result = $this->postOutside($url, [], [ 'account' => $account, 'password' => $password ]);
 		return $result;
 	}
 	

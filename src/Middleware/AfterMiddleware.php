df86128c40939734e53331f7da48b01768f31c4f
zhangyubo <zhangyubo@192.168.5.140>
2017-07-26 13:51:15 +0800

    1.1.99

--- src/Middleware/AfterMiddleware.php
+++ src/Middleware/AfterMiddleware.php
@@ -12,33 +12,34 @@ class AfterMiddleware
 	{
 		$response = $next($request);
 		
-		$content = $data= $response->getContent();
-		if (is_string($data)) {
-			$data = json_decode($data, true);
+		$content = $response->getContent();
+		if (is_string($content)) {
+			$data = json_decode($content, true);
 		}
-		$time = DateHelper::microtime_float();
-		$data['run_time'] = $time - TIME_START;
-		
-		if (is_array($data) && isset($data['code']) && $data['code']>0 && empty($data['msg'])) {
-			$code = $data['code'];
-			$lang = $request->input('app_lang') ? : 'zh_cn';
-			$lang = $lang ? $lang : 'zh_cn';
-			$error_code = OptionHelper::getOption('error_code','options',$lang);
-			$msg = isset($error_code[$code]) ? $error_code[$code] : '未知错误';
+		if (isset($data) && is_array($data)) {
 			
-			if(isset($data['data'])){
-				foreach ($data['data'] as $key => $value) {
-					if (is_string($value)) {
-						$msg=str_replace("{".$key."}", $value, $msg);
+			if (isset($data['code']) && $data['code']>0 && empty($data['msg'])) {
+				
+				$code = $data['code'];
+				$lang = $request->input('app_lang') ? : 'zh_cn';
+				$lang = $lang ? $lang : 'zh_cn';
+				$error_code = OptionHelper::getOption('error_code','options',$lang);
+				$msg = isset($error_code[$code]) ? $error_code[$code] : '未知错误';
+				if(isset($data['data'])){
+					foreach ($data['data'] as $key => $value) {
+						if (is_string($value)) {
+							$msg=str_replace("{".$key."}", $value, $msg);
+						}
 					}
 				}
+				$time = DateHelper::microtime_float();
+				$data['run_time'] = $time - TIME_START;
+				$data['code'] = strval($code);
+				$data['msg'] = $msg;
 			}
-			$data['code'] = strval($code);
-			$data['msg'] = $msg;
+			$content = json_encode($data);
 		}
-		if (is_array($data)) {
-		  $content = json_encode($data);
-		}
+		
 		if ($request->input('source') == 2 || $request->input('source') == 3 && !defined('REQUEST_IS_LOCAL')) {
 			//加密
 			$key=substr(md5(env('AES_KEY')."gou"),0,8);

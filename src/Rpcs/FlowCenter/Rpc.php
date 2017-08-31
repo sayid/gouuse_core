33d9ca3b97668560888793d005cb187738438bed
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-08-31 14:09:37 +0800

    流程rpc

---
+++ src/Rpcs/FlowCenter/Rpc.php
@@ -0,0 +1,18 @@
+<?php
+namespace GouuseCore\Rpcs\FlowCenter;
+
+use GouuseCore\Rpcs\BaseRpc;
+
+/**
+ * API SDK基类
+ * @author zhangyubo
+ *
+ */
+class Rpc extends BaseRpc
+{
+	protected $host_pre = '/flow_center/';
+	
+	protected $service_id = 1004;
+	
+	
+}

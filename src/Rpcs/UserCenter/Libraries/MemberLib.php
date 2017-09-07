5e3221e9a9efee9df56712e41b9476bd7f06c9b8
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-09-07 09:41:16 +0800

    用户中心方法优化

--- src/Rpcs/UserCenter/Libraries/MemberLib.php
+++ src/Rpcs/UserCenter/Libraries/MemberLib.php
@@ -1,6 +1,7 @@
 <?php
 namespace GouuseCore\Rpcs\UserCenter\Libraries;
 
+use GouuseCore\Helpers\OptionHelper;
 use GouuseCore\Rpcs\UserCenter\Rpc;
 
 class MemberLib extends Rpc
@@ -52,4 +53,102 @@ class MemberLib extends Rpc
 		
 		return $member_result;
 	}
+
+
+    /**
+     * 获取用户信息
+     * @param $extra   查询条件-需要包含公司id，用户状态默认是在职用户
+     * @param string   $need_field  用户需要查询的字段，默认为空为全部
+     * @return array
+     */
+    public function getMemberList($extra, $need_field = '')
+    {
+        if (!empty($need_field)) {
+            $need_field = explode(',', $need_field);
+        }
+        $company_id = $extra['company_id'] ?? 0;
+        $status = $extra['status'] ?? 1;
+        $member_list = $this->MemberModel->getAllSimple($company_id, $status);
+        // 获取部门列表
+        $department_list = $this->do('DepartmentModel', 'getAllWithId', [$company_id]);
+        // 获取职位列表
+        $position_list = $this->do('CompanyPositionLib', 'getNameList', [$company_id]);
+        // 转换
+        $member_opt = OptionHelper::getOption('member');
+        $sex = $member_opt['sex'];
+        $blood_type = $member_opt['blood_type'];
+        $list = [];
+        if (!empty($member_list)) {
+            foreach ($member_list as $key => $val) {
+                // 处理用户数据
+                $val['department_name'] = $department_list[$val['department_id']]['department_name'] ?? '';
+                $val['position_name'] = $position_list[$val['position_id']] ?? '';
+                $val['sex_text'] = $sex[$val['sex']] ?? '';
+                $val['blood_type_text'] = $blood_type[$val['blood_type']] ?? '';
+                // 过滤
+                // 部门查询
+                if (!empty($extra['department_id'])) {
+                    $department_ids = explode(',', $extra['department_id']);
+                    if (!in_array($val['department_id'], $department_ids)) {
+                        continue;
+                    }
+                }
+                // 角色id查询
+                if (isset($extra['role_id'])) {
+                    $role_ids = explode(',', $extra['role_id']);
+                    $in_role = false;
+                    if (!empty($val['role_group'])) {
+                        $role_info = json_decode($val['role_group'], true);
+                        foreach ($role_info as $role_id) {
+                            if (array_intersect($role_ids, $role_id)) {
+                                $in_role = true;
+                            }
+                        }
+                    }
+                    if (!$in_role) {
+                        continue;
+                    }
+                }
+                // 职位id查询
+                if (!empty($extra['position_id'])) {
+                    $position_ids = explode(',', $extra['position_id']);
+                    if (!in_array($val['position_id'], $position_ids)) {
+                        continue;
+                    }
+                }
+                // 用户id查询
+                if (!empty($extra['member_id'])) {
+                    $member_ids = explode(',', $extra['member_id']);
+                    if (!in_array($val['member_id'], $member_ids)) {
+                        continue;
+                    }
+                }
+                // 过滤前面已经查询了的条件
+                $filter_field = 'member_id,company_id,position_id,status,role_id,department_id';
+                $search_extra = ArrayHelper::filterArray($filter_field, $extra, 'nokeep');
+                $match = true;
+                if (!empty($search_extra)) {
+                    foreach ($search_extra as $search_key => $search_value) {
+                        if (array_key_exists($search_key, $val) == false) {
+                            continue;
+                        } else {
+                            if ($val[$search_key] != $search_value) {
+                                $match = false;
+                                continue;
+                            }
+                        }
+                    }
+                }
+                if ($match == false) {
+                    continue;
+                }
+                // 过滤字段
+                if (!empty($need_field)) {
+                    $val = ArrayHelper::filterArray($need_field, $val);
+                }
+                $list[] = $val;
+            }
+        }
+        return $list;
+    }
 }

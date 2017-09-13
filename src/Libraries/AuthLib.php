ffb2aac489593a840c08d9f8df0a6214979c318f
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-09-13 10:41:49 +0800

    修改核心

--- src/Libraries/AuthLib.php
+++ src/Libraries/AuthLib.php
@@ -104,6 +104,61 @@ class AuthLib extends Lib
     }
     
     /**
+     * 获取应用对应权限
+     * @param 应用id $app_id
+     * @param 用户id $member_id
+     * @param 权限字段 $field set_permission:设置权限,data_permission:数据权限,admin_permission:管理权限,reimburse_permission:报销存根权限
+     *   $field  eg: "set_permission, admin_permission" 多个字段用逗号分隔
+     */
+    public function needMemberAppSetAuth($app_id, $member_id, $field)
+    {
+        $field = explode(',', $field);
+        foreach ($field as $item) {
+            if (!in_array(trim($item), ['set_permission', 'data_permission', 'admin_permission', 'reimburse_permission'])) {
+                return ['code' => CodeLib::AUTH_PARAM_ERROR];
+            }
+        }
+    
+        if (empty($this->member_info['member_id'])) {
+            return ['code' => CodeLib::AUTH_DENY];
+        }
+        if (!isset($this->member_info['manage_apps'])) {
+            return ['code' => CodeLib::AUTH_DENY];
+        }
+        $app_ids = array_column($this->member_info['manage_apps'], 'app_id');
+        if (empty($app_ids)) {
+            return ['code' => CodeLib::AUTH_DENY];
+        }
+        if (!in_array($app_id, $app_ids)) {
+            //当前用户不能管理该应用
+            return ['code' => CodeLib::AUTH_DENY];
+        }
+    
+        foreach ($this->member_info['manage_apps'] as $val) {
+            if ($val['app_id'] == $app_id) {
+                $app_info = $val;
+            }
+        }
+    
+        if (!isset($app_info) || empty($app_info)) {
+            return ['code' => CodeLib::AUTH_DENY];
+        }
+        $auth = false;
+        foreach ($field as $row) {
+            if($app_info[trim($row)] == 1) {
+                $auth = true;
+            } else {
+                $auth = false;
+            }
+        }
+        //应用超管有权限
+        if ($auth === true || $app_info['super_manage'] == 1) {
+            return ['code' => 0];
+        }
+        return ['code' => CodeLib::AUTH_DENY];
+    }
+    
+    /**
      * 平台管理员
      * @return number
      */

4d5148f4b1047fa4e490ed2222c53751917daac5
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-09-15 11:59:53 +0800

    rpc

---
+++ src/Libraries/AppLib.php
@@ -0,0 +1,11 @@
+<?php
+namespace GouuseCore\Libraries;
+
+class AppLib
+{
+    const MEMBER_APP_ID = 1;//员工管理
+    const MEMBER_CONTACTS_APP_ID = 2;//企业通讯录
+    const FLOW_APP_ID = 3;//审批
+    const ATTENDENCE_APP_ID = 4;//考勤
+    
+}

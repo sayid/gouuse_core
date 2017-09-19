718048fd776f74517fe511fbd29c42a5a86bbb3e
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-09-19 14:53:26 +0800

    邮件中心

---
+++ src/Rpcs/EmailCenter/Rpc.php
@@ -0,0 +1,20 @@
+<?php
+namespace GouuseCore\Rpcs\MessageCenter;
+
+use GouuseCore\Rpcs\BaseRpc;
+
+/**
+ * API SDK基类
+ * @author zhangyubo
+ *
+ */
+class Rpc extends BaseRpc
+{
+	protected $host_pre = '/email_service/';
+	
+	protected $service_id = 1002;
+	
+	//私有host，各个服务可以自己定义不一样的host地址
+	private $_private_host = '';
+	
+}

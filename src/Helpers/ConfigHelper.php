eb002ac91b22e854da74649db230cb14e73f20b5
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-09-22 14:56:36 +0800

    增加调试

--- src/Helpers/ConfigHelper.php
+++ src/Helpers/ConfigHelper.php
@@ -7,40 +7,40 @@ class ConfigHelper
 {
     /**
      * 加载选项文件
-    * @param  [type] $option_name [description]
-    * @return [array]              [description]
-    */
-    public static function getConfig($option_name, $dir = "", $lang_name = "zh_cn")
+     * @param  [type] $option_name [description]
+     * @return [array]              [description]
+     */
+    public static function getConfig($option_name, $dir = "")
     {
-        if ($lang_name == "" && defined('DEFAULT_LANG_NAME')) {
-            $lang_name = DEFAULT_LANG_NAME;
-        }
-        if ($lang_name) {
-            $cache_key = "x_system_get_options_".$option_name."_".$dir."_".$lang_name;
-        } else {
-            $cache_key = "x_system_get_options_".$option_name."_".$dir;
-        }
-    
+        $cache_key = env('SERVICE_ID').":x_system_get_options_".$option_name."_".$dir;
+
         $cache_data = Cache::get($cache_key);
-    
+
         if (!$cache_data) {
-        	
+
             $sub_dir = "";
-    
+
+            $option_name= str_replace(["//",".."], "",  $option_name);
+
+            $option_name= str_replace(".", "/", $option_name);
+
             $dir = str_replace(["//",".."], "",  $dir);
-            
+
             $dir = str_replace(".", "/", $dir);
-            
+
             $array_file = array();
-    
+
             $array_option_name = explode("_", $option_name);
             $option_name_first = array_shift($array_option_name);
             $option_name_end = join("_", $array_option_name);
             $root_path = substr(__DIR__, 0, -12);
-            $root_path .  "/resources/".$dir."/".$option_name.".php";
-            return require_once $root_path .  "/resources/options/".$dir."/".$option_name.".php";
+            $path = $root_path .  "/resources/options/".$dir."/".$option_name.".php";
+            if (!is_file($path)) {
+                $path = $root_path .  "/resources/options/".$dir."/".$option_name.".inc.php";
+            }
+            return require_once $path;
         }
-        
+
         return $cache_data;
     }
 }

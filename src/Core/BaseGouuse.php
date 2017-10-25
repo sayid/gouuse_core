e715d577af708fae259ac9ed39a0cb6a6df50f47
张玉波 <zhangyubo@zybdeMacBook-Pro.local>
2017-10-25 11:59:13 +0800

    优化登录用户数据，共享member_info,company_info

--- src/Core/BaseGouuse.php
+++ src/Core/BaseGouuse.php
@@ -4,6 +4,7 @@ namespace GouuseCore\Core;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 
 /**
  * @author zhangyubo
@@ -16,7 +17,18 @@ class BaseGouuse
 	public function __construct($params = [])
 	{
 	}
-	
+
+    public function __set($key, $value)
+    {
+        if ($key == 'member_info' || $key == 'company_info') {
+            $key = $key == 'member_info' ? 'GOUUSE_MEMBER_INFO' : 'GOUUSE_COMPANY_INFO';
+            Config::set($key, $value);
+        } else {
+            Config::set($key, $value);
+        }
+        $this->{$key} = $value;
+    }
+
 	public function __get($class)
 	{
 		
@@ -24,12 +36,14 @@ class BaseGouuse
 			if (!defined('NEED_AUTH_CHECK')) {
 				return null;
 			}
+            return Config::get('GOUUSE_MEMBER_INFO');
 			if (defined('GOUUSE_MEMBER_INFO')) {
 				return GOUUSE_MEMBER_INFO;
 			}
 			//define('GOUUSE_MEMBER_INFO', Auth::user());
 			//return GOUUSE_MEMBER_INFO;
 		} else if ($class == 'company_info') {
+            return Config::get('GOUUSE_COMPANY_INFO');
 			if (defined('GOUUSE_COMPANY_INFO')) {
 				return GOUUSE_COMPANY_INFO;
 			}

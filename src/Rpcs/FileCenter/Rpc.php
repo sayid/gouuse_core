190060f1e515b24a6ed4a6dcd62723f84d867d55
wulei <838335161@qq.com>
2017-10-26 15:16:21 +0800

    新增文件rpc

---
+++ src/Rpcs/FileCenter/Rpc.php
@@ -0,0 +1,27 @@
+<?php
+namespace GouuseCore\Rpcs\UserCenter;
+
+use GouuseCore\Rpcs\BaseRpc;
+
+
+/**
+ * API SDK基类
+ * @author zhangyubo
+ *
+ */
+class Rpc extends BaseRpc
+{
+	
+	protected $host_pre = '/file_service/';
+	
+	protected $service_id = 1000;
+	
+	//私有host，各个服务可以自己定义不一样的host地址
+	protected $_private_host = '';
+	
+	public function __construct()
+	{
+		
+	}
+	
+}
